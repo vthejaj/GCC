6643a2e51a4a93990f468e6466a61400735b1a95
vishnuthejaj <vishnutheja@hotmail.com>
2024-05-29 18:18:20 +0530

    Created GCC_setup

---
+++ GCC_setup/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int x;
+    //cout << "Enter value of x: ";
+    cin >> x;
+    cout << "Value of x: " << x << endl;
+    cout << "cplusplus version: " << __cplusplus << endl;
+    return 0;
+}
